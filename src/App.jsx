950b41684a5665d67fd38b04e9e479893738f71b
JQuilodranDev <jaime.quilodrandev@gmail.com>
2021-10-13 19:29:28 -0300

    feat(Root): Start React TikTok clone project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+    <main>
+    <h3>React Tik Tok</h3>
+    </main>
+    </div>
+  )
+}
+
+export default App
